63f1ba7d47b8ca57509390b106835574f286c727
Adam Reese <areese801@gmail.com>
2023-07-16 15:56:13 -0600

    make xcall.py work with python3

--- xcall.py
+++ xcall.py
@@ -24,7 +24,7 @@ and the chance of replies being mixed up.
 """
 
 import json
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import logging
 import os
 import subprocess
@@ -130,7 +130,7 @@ class XCallClient(object):
         cmdurl = self._build_url(action, action_parameters)
         logger.debug('--> ' + cmdurl)
         result = self._xcall(cmdurl, activate_app)
-        logger.debug('<-- ' + unicode(result) + '\n')
+        logger.debug('<-- ' + str(result) + '\n')
 
         return result
 
@@ -141,9 +141,9 @@ class XCallClient(object):
 
         if action_parameter_dict:
             par_list = []
-            for k, v in action_parameter_dict.iteritems():
+            for k, v in list(action_parameter_dict.items()):
                 par_list.append(
-                    k + '=' + urllib.quote(unicode(v).encode('utf8')))
+                    k + '=' + urllib.parse.quote(str(v).encode('utf8')))
             url = url + '?' + '&'.join(par_list)
         return url
 
@@ -154,10 +154,12 @@ class XCallClient(object):
 
         logger.info('Making bash call: "%s"' % ' '.join(args))
 
-
         p = subprocess.Popen(
             args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = p.communicate()
+
+        stdout = stdout.decode('utf-8')
+        stderr = stderr.decode('utf-8')
 
         # Assert that reply had output on one, and only one of stdout and stderr
         if (stdout != '') and (stderr != ''):
@@ -171,7 +173,8 @@ class XCallClient(object):
                 'Try xcall directly from terminal with: "%s"' % ' '.join(args))
 
         if stdout:
-            response = urllib.unquote(stdout).decode('utf8')
+            # response = urllib.parse.unquote(stdout).decode('utf8')
+            response = urllib.parse.unquote(stdout)
             if self.json_decode_success:
                 return json.loads(response)
             else:
